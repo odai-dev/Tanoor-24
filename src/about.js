c528063eff4775ac86bcc9cb56fb640a0ea50b55
Odai Gubran <95129948+odai-dev@users.noreply.github.com>
2025-10-17 20:29:41 +0300

    fixed minor bugs

--- src/about.js
+++ src/about.js
@@ -19,7 +19,7 @@ export const loadAbout = ()=> {
     aboutDiv.appendChild(line2);
 
     const p2 = document.createElement("p");
-    p2.innerText = "We are a tribute to the timless flavors of Yemeni cusine, built for the 24-hour hustle. <br>Tanoor 24 brings the deep, smoky taste of food cooked in the traditional tanoor clay oven to your table with unparalleled speed and consistency.";
+    p2.innerText = "The Tanoor is the heart of our kitchen. It’s an ancient technique that slow-cooks the finest meats and bakes our Khubz (bread) to perfection, locking in authentic flavor. We honor this tradition, ensuring every dish—from Mandi to Madfoon—is an experience";
     aboutDiv.appendChild(p2);
 
     const line3 =  document.createElement("h2");
@@ -27,7 +27,7 @@ export const loadAbout = ()=> {
     aboutDiv.appendChild(line3);
 
     const p3 = document.createElement("p");
-    p3.innerText = "We are a tribute to the timless flavors of Yemeni cusine, built for the 24-hour hustle. <br>Tanoor 24 brings the deep, smoky taste of food cooked in the traditional tanoor clay oven to your table with unparalleled speed and consistency.";
+    p3.innerText = "Why \"24\"? Because great food shouldn't wait. We deliver premium, authentic meals when you need them. Whether it's a quick lunch or a late-night feast, Tanoor 24 is always ready, always hot, and always committed to quality.";
     aboutDiv.appendChild(p3);
 
 
